54f6aa479d0fd5a65178029cb5d59984263d4513
徐细洲 <xuxizhou1994@163.com>
2017-11-23 16:27:13 +0800

    Initial commit

---
+++ src/main/java/com/sericefeign/feign/FeignApplication.java
@@ -0,0 +1,18 @@
+package com.sericefeign.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+@EnableHystrixDashboard
+public class FeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignApplication.class, args);
+	}
+}
